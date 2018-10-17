a06a82488d526cbcd99380f14c351fa644436589
Walter van der Giessen <waltervdgiessen@gmail.com>
2018-10-17 10:57:49 +0200

    Bugfixing

--- dist/js/tool.js
+++ dist/js/tool.js
@@ -352,7 +352,7 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
          */
         saveProfile: function () {
             var _ref3 = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_babel_runtime_regenerator___default.a.mark(function _callee2() {
-                var response;
+                var response, success_message_locale;
                 return __WEBPACK_IMPORTED_MODULE_0_babel_runtime_regenerator___default.a.wrap(function _callee2$(_context2) {
                     while (1) {
                         switch (_context2.prev = _context2.next) {
@@ -368,23 +368,24 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 
                                 this.loading = false;
 
-                                // if (__("nova-profile-settings#success_message") != 'nova-profile-settings#success_message' && __("nova-profile-settings#success_message") != false) {
-                                //     var success_message_locale = __("nova-profile-settings#success_message");
-                                // } else { // No translation specified: fallback to EN
-                                //     var success_message_locale = "Profile has been updated!";
-                                // }
+                                if (__("nova-profile-settings#success_message") != 'nova-profile-settings#success_message' && __("nova-profile-settings#success_message") != false) {
+                                    success_message_locale = __("nova-profile-settings#success_message");
+                                } else {
+                                    // No translation specified: fallback to EN
+                                    success_message_locale = "Profile has been updated!";
+                                }
 
-                                this.$toasted.show(this.__("nova-profile-settings#success_message"), { type: 'success' });
+                                this.$toasted.show(success_message_locale, { type: 'success' });
 
                                 // Reset the form by refetching the fields
                                 this.getFields();
 
                                 this.validationErrors = new __WEBPACK_IMPORTED_MODULE_1_laravel_nova__["Errors"]();
-                                _context2.next = 15;
+                                _context2.next = 16;
                                 break;
 
-                            case 11:
-                                _context2.prev = 11;
+                            case 12:
+                                _context2.prev = 12;
                                 _context2.t0 = _context2['catch'](0);
 
                                 this.loading = false;
@@ -392,12 +393,12 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
                                     this.validationErrors = new __WEBPACK_IMPORTED_MODULE_1_laravel_nova__["Errors"](_context2.t0.response.data.errors);
                                 }
 
-                            case 15:
+                            case 16:
                             case 'end':
                                 return _context2.stop();
                         }
                     }
-                }, _callee2, this, [[0, 11]]);
+                }, _callee2, this, [[0, 12]]);
             }));
 
             function saveProfile() {
